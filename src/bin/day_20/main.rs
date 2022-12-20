bb6ec81144479bc2343aedcaa8f78f1931bbe749
Jens Plüddemann <jens@plueddemann.de>
2022-12-20 18:33:39 +0100

    day 20

--- src/bin/day_20/main.rs
+++ src/bin/day_20/main.rs
@@ -6,45 +6,38 @@ const INPUT: &str = include_str!("input");
 const DECRYPTION_KEY: &str = include_str!("decryption_key");
 
 #[derive(Debug)]
-struct Encryption<'a> {
-  data: &'a Vec<i64>,
+struct Decryption {
+  data: Vec<i64>,
   positions: VecDeque<usize>,
 }
 
-impl<'a> Encryption<'a> {
-  pub fn new(data: &'a Vec<i64>) -> Self {
+impl Decryption {
+  pub fn new(data: &Vec<i64>, decryption_key: &i64) -> Self {
+    let data = data.iter().map(|&x| x * decryption_key).collect::<Vec<_>>();
     let positions = (0..data.len()).collect::<VecDeque<usize>>();
     Self { data, positions }
   }
 
-  fn mix(&mut self) {
+  fn mix(&mut self, iterations: usize) {
     let modulus = self.positions.len() - 1;
-    for (idx, val) in self.data.iter().enumerate() {
-      if *val == 0 {
-        continue;
+    (0..iterations).for_each(|_| {
+      for (i, &x) in self.data.iter().enumerate() {
+        let current_position = self.positions.iter().position(|&y| y == i).unwrap();
+        self.positions.remove(current_position);
+        let new_index =
+          (current_position as i64 + x).rem_euclid(self.positions.len() as i64) as usize;
+        self.positions.insert(new_index, i)
       }
-      let current_position = self.positions.iter().position(|&x| x == idx).unwrap();
-      let old_position = self.positions.remove(current_position).unwrap();
-      let new_val = val.unsigned_abs() as usize % modulus;
-
-      if *val < 0 {
-        self.positions.rotate_right(new_val);
-      } else {
-        self.positions.rotate_left(new_val);
-      }
-      self.positions.insert(current_position, old_position);
-    }
+    });
   }
 
   fn coordinates(&self) -> Vec<i64> {
     let idx_0_old = self.data.iter().position(|&x| x == 0).unwrap();
     let idx_0_new = self.positions.iter().position(|&x| x == idx_0_old).unwrap();
 
-    (1..=3)
-      .map(|i| {
-        let current_position = (i * 1000 + idx_0_new) % self.positions.len();
-        self.data[self.positions[current_position]]
-      })
+    [1000, 2000, 3000]
+      .iter()
+      .map(|i| self.data[self.positions[(idx_0_new + i) % self.positions.len()]])
       .collect()
   }
 }
@@ -55,19 +48,18 @@ fn parse(input: &str) -> Vec<i64> {
 
 pub(crate) fn part_1(input: &str) -> i64 {
   let data = parse(input);
-  let mut encryption = Encryption::new(&data);
+  let mut encryption = Decryption::new(&data, &1);
 
-  encryption.mix();
+  encryption.mix(1);
   encryption.coordinates().iter().sum()
 }
 
 pub(crate) fn part_2(input: &str) -> i64 {
   let decryption_key = DECRYPTION_KEY.parse::<i64>().unwrap();
-  let data = parse(input).iter().map(|&x| x * decryption_key).collect();
-  let mut encryption = Encryption::new(&data);
+  let data = parse(input);
+  let mut encryption = Decryption::new(&data, &decryption_key);
 
-  (0..10).for_each(|_| encryption.mix());
-
+  encryption.mix(10);
   encryption.coordinates().iter().sum()
 }
 
