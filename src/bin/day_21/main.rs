274c72006ff7e3dd3bab6314040a5c274ea74d34
Jens Plüddemann <jens@plueddemann.de>
2022-12-21 12:35:36 +0100

    day 21 part two

--- src/bin/day_21/main.rs
+++ src/bin/day_21/main.rs
@@ -1,12 +1,15 @@
 #![allow(unused_variables)]
 
-use std::collections::{BTreeMap, HashMap};
+use std::{
+  cmp::Ordering,
+  collections::{BTreeMap, HashMap},
+};
 
 const INPUT: &str = include_str!("input");
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Yell<'a> {
-  Number(u32),
+  Number(i128),
   Add(&'a str, &'a str),
   Sub(&'a str, &'a str),
   Mul(&'a str, &'a str),
@@ -38,51 +41,78 @@ fn parse(input: &str) -> BTreeMap<&str, Yell> {
     })
 }
 
-pub(crate) fn part_1(input: &str) -> i64 {
+fn evaluate<'a>(
+  monkey: &'a str,
+  yells: &'a BTreeMap<&'a str, Yell>,
+  cache: &mut HashMap<&'a str, i128>,
+) -> i128 {
+  if let Some(val) = cache.get(monkey) {
+    return *val;
+  };
+
+  let val = match yells.get(monkey).unwrap() {
+    Yell::Number(val) => *val,
+    Yell::Add(lhs, rhs) => evaluate(lhs, yells, cache) + evaluate(rhs, yells, cache),
+    Yell::Sub(lhs, rhs) => evaluate(lhs, yells, cache) - evaluate(rhs, yells, cache),
+    Yell::Mul(lhs, rhs) => evaluate(lhs, yells, cache) * evaluate(rhs, yells, cache),
+    Yell::Div(lhs, rhs) => evaluate(lhs, yells, cache) / evaluate(rhs, yells, cache),
+  };
+
+  cache.insert(monkey, val);
+  val
+}
+
+pub(crate) fn part_1(input: &str) -> i128 {
   let yells = parse(input);
-
-  fn calculate_yell<'a>(
-    monkey: &'a str,
-    monkeys: &'a BTreeMap<&'a str, Yell>,
-    cache: &mut HashMap<&'a str, i64>,
-  ) -> i64 {
-    if let Some(val) = cache.get(monkey) {
-      return *val;
-    };
-
-    let val = match monkeys.get(monkey).unwrap() {
-      Yell::Number(val) => (*val).into(),
-      Yell::Add(lhs, rhs) => {
-        calculate_yell(lhs, monkeys, cache) + calculate_yell(rhs, monkeys, cache)
-      }
-      Yell::Sub(lhs, rhs) => {
-        calculate_yell(lhs, monkeys, cache) - calculate_yell(rhs, monkeys, cache)
-      }
-      Yell::Mul(lhs, rhs) => {
-        calculate_yell(lhs, monkeys, cache) * calculate_yell(rhs, monkeys, cache)
-      }
-      Yell::Div(lhs, rhs) => {
-        calculate_yell(lhs, monkeys, cache) / calculate_yell(rhs, monkeys, cache)
-      }
-    };
-
-    cache.insert(monkey, val);
-    val
-  }
-
   let mut cache = HashMap::new();
-  calculate_yell("root", &yells, &mut cache)
+  evaluate("root", &yells, &mut cache)
+}
+
+fn binary_search<F>(mut lo: i128, mut hi: i128, mut cmp: F) -> Option<i128>
+where
+  F: FnMut(i128) -> Ordering,
+{
+  while lo != hi {
+    let mid = (lo + hi) / 2;
+    match cmp(mid) {
+      Ordering::Equal => hi = mid,
+      Ordering::Less => hi = mid - 1,
+      Ordering::Greater => lo = mid + 1,
+    }
+  }
+  match cmp(lo) {
+    Ordering::Equal => Some(lo),
+    Ordering::Less => None,
+    Ordering::Greater => None,
+  }
 }
 
 pub(crate) fn part_2(input: &str) -> i128 {
-  fn calculate_yell(
-    idx: &usize,
-    monkey: &BTreeMap<usize, Yell>,
-    cache: HashMap<usize, i128>,
-  ) -> i128 {
-    todo!()
-  }
-  todo!()
+  let mut yells = parse(input);
+  let mut cache = HashMap::new();
+
+  let (lhs, rhs) = match *yells.get("root").unwrap() {
+    Yell::Add(lhs, rhs) => (lhs, rhs),
+    Yell::Sub(lhs, rhs) => (lhs, rhs),
+    Yell::Mul(lhs, rhs) => (lhs, rhs),
+    Yell::Div(lhs, rhs) => (lhs, rhs),
+    _ => unreachable!(),
+  };
+
+  let start = evaluate(lhs, &yells, &mut cache).cmp(&evaluate(rhs, &yells, &mut cache));
+  binary_search(0, i64::MAX as i128, move |x| {
+    let mut cache = HashMap::new();
+    yells.insert("humn", Yell::Number(x));
+    let next = evaluate(lhs, &yells, &mut cache).cmp(&evaluate(rhs, &yells, &mut cache));
+    match next {
+      Ordering::Equal => Ordering::Equal,
+      next => match start == next {
+        true => Ordering::Greater,
+        false => Ordering::Less,
+      },
+    }
+  })
+  .unwrap()
 }
 
 fn main() {
@@ -103,9 +133,8 @@ pub(crate) mod tests {
   }
 
   #[test]
-  #[ignore = "later"]
   fn test_solve_part_2() {
     let res = part_2(TEST_INPUT);
-    assert_eq!(res, 0)
+    assert_eq!(res, 301)
   }
 }
